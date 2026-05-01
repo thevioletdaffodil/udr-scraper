9c5217c5b70a43d7437422dcac50bdd95f2373c5
Ramona Singh <157743989+thevioletdaffodil@users.noreply.github.com>
2026-05-02 00:12:47 +0530

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -4,7 +4,7 @@ from scraper import main_logic # Or just paste the scraper code here
 
 app = func.FunctionApp()
 
-@app.timer_trigger(schedule="0 0 * * *", arg_name="myTimer", run_on_startup=False)
+@app.timer_trigger(schedule="0 0 * * *", arg_name="myTimer", run_on_startup=True)
 def timer_trigger(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('The timer is past due!')
